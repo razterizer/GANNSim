3fe75bd6c3f011f19379aa5ca1b046ad8c460745
razterizer <scratty@gmail.com>
2024-04-18 17:59:31 +0200

    Better visualization of children when only showing relevant individuals.

--- GANNSim/GANNSim/species/Population.cs
+++ GANNSim/GANNSim/species/Population.cs
@@ -292,12 +292,21 @@ namespace GANNSim.species
                     0, m_population.Count,
                     debug_draw_joints, debug_draw_angsprings,
                     draw_dna, draw_name);
+                int layer_offs = 5;
+                int num_layers = 1 + layer_offs;
+                int layer_idx = num_layers;
+                for (int individual_idx = 1; individual_idx < m_population.Count; individual_idx++)
+                {
+                    Individual individual = m_population[individual_idx];
+                    if (individual.Genome.IsChild || !individual.IsSimulated)
+                        num_layers++;
+                }
                 for (int individual_idx = 1; individual_idx < m_population.Count; individual_idx++)
                 {
                     Individual individual = m_population[individual_idx];
                     if (individual.Genome.IsChild || !individual.IsSimulated)
                         individual.Draw(g,
-                            individual_idx, m_population.Count,
+                            layer_idx++, num_layers,
                             debug_draw_joints, debug_draw_angsprings,
                             draw_dna, draw_name);
                 }
